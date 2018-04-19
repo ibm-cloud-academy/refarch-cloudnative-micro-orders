99d1760bb3d999ee3d421a159d2efc1cb8394d57
Budi Darmawan <vbudi@us.ibm.com>
2018-04-19 13:43:19 -0500

    update create db

--- mysql/create_orders_db.sh
+++ mysql/create_orders_db.sh
@@ -13,19 +13,19 @@ if [ -z "${MYSQL_ROOT_PASSWORD}" ]; then
 fi
 
 echo "Creating database ${MYSQL_ORDERS_DATABASE} ..."
-mysql -uroot ${_password_opt} <<EOF
+mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -uroot ${_password_opt} <<EOF
 create database if not exists ${MYSQL_ORDERS_DATABASE};
 EOF
 
 echo "Creating database user ${MYSQL_ORDERS_USER} ..."
-mysql -uroot ${_password_opt} <<EOF
+mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -uroot ${_password_opt} <<EOF
 create user if not exists '${MYSQL_ORDERS_USER}'@'%' identified by '${MYSQL_ORDERS_PASSWORD}';
 grant all on ${MYSQL_ORDERS_DATABASE}.* to '${MYSQL_ORDERS_USER}'@'%';
 EOF
 
 echo "Creating orders table ..."
 
-mysql -uroot ${_password_opt} <<EOF
+mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -uroot ${_password_opt} <<EOF
 use ${MYSQL_ORDERS_DATABASE};
 source create_orders_table.sql;
 EOF
